2970559bc61d202d065b9fd90e65ae2e4ed8ca0a
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-10-22 18:29:37 +0530

    Added Gitignore

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
- access_token = var.access_token
- #credentials = file("../key.json")
+ #access_token = var.access_token
+ credentials = file("../key.json")
 }
 
 resource "google_pubsub_topic" "example" {
@@ -24,4 +24,70 @@ resource "google_bigquery_dataset" "dataset" {
     env = "default"
   }
 
+}
+
+resource "google_dataproc_cluster" "mycluster" {
+  name     = "mycluster"
+  region   = "us-central1"
+  graceful_decommission_timeout = "120s"
+  
+  labels = {
+    env = "dev"
+    foo = "bar"
+  }
+
+  cluster_config {
+    staging_bucket = "dataproc-staging-bucket"
+
+    master_config {
+      num_instances = 1
+      machine_type  = "e2-medium"
+      disk_config {
+        boot_disk_type    = "pd-ssd"
+        boot_disk_size_gb = 30
+      }
+    }
+
+    worker_config {
+      num_instances    = 2
+      machine_type     = "e2-medium"
+      min_cpu_platform = "Intel Skylake"
+      disk_config {
+        boot_disk_size_gb = 30
+        num_local_ssds    = 1
+      }
+    }
+
+    preemptible_worker_config {
+      num_instances = 0
+    }
+
+    # Override or set some custom properties
+    software_config {
+      image_version = "1.3.7-deb9"
+      override_properties = {
+        "dataproc:dataproc.allow.zero.workers" = "true"
+      }
+    }
+
+  }
+}
+
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "secret"
+
+  labels = {
+    label = "my-label"
+  }
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+      replicas {
+        location = "us-east1"
+      }
+    }
+  }
 }
